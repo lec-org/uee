fa8707341c6c5802e4ecf7410c9ca89c6ebd9a5a
陈信豪 <3460213296@qq.com>
2023-12-01 22:34:24 +0800

    添加了Editor组件

--- src/components/SingleProblem/content/Editor/index.tsx
+++ src/components/SingleProblem/content/Editor/index.tsx
@@ -1,3 +1,27 @@
+import { useState } from 'react'
+import MonacoEditor from 'react-monaco-editor'
+
+import styles from './index.module.scss'
+
 export default function Editor() {
-	return <></>
+	const [code, setCode] = useState('')
+	const options = {
+		selectOnLineNumbers: true,
+	}
+	const handleCodeChange = (e: string) => {
+		setCode(e)
+	}
+	return (
+		<div className={styles['monacoEditorContainer']}>
+			<MonacoEditor
+				width={'100%'}
+				height={'100%'}
+				language='javascript'
+				theme='vs-dark'
+				value={code}
+				options={options}
+				onChange={handleCodeChange}
+			/>
+		</div>
+	)
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+// import './userWorker.ts'
 import './index.css'
 import App from './App.tsx'
 
